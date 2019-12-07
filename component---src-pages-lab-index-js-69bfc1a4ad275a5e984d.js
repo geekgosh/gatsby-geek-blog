52529afa87808d49ee56035e669ab5465c5374e5
shengchuang-wong <shengchuang1996@gmail.com>
2019-12-07 23:19:55 +0800

    some new post

---
+++ component---src-pages-lab-index-js-69bfc1a4ad275a5e984d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{YMUj:function(e,t,a){"use strict";a.r(t);var i=a("q1tI"),n=a.n(i);t.default=function(e){return n.a.createElement("img",{style:{imageRendering:"pixelated"},src:"https://www.allaboutvision.com/static/cda54d1ced3945f821bb4fd3a1b49ff9/e3b6c/jack-russell-blurred-1200x630.jpg",alt:"forest",width:500,height:500})}}}]);
+//# sourceMappingURL=component---src-pages-lab-index-js-69bfc1a4ad275a5e984d.js.map
